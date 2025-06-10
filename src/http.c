30bca54818bdbc0d4abeca10120857e4e7ad62a5
Will Bolton <git@bolton.me.uk>
2025-06-10 09:03:17 +0100

    Add HTTP headers to response

--- src/http.c
+++ src/http.c
@@ -1,13 +1,16 @@
 #define	MAX_RESPONSE	1024
 
-#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "http.h"
 
 static char RES_BUF[MAX_RESPONSE] = "HTTP/1.1 ";
 static const int RES_BUF_START = 9;
 static const char *CRLF = "\r\n";
+static const char *SERVER_TOKEN = "ihttpd/0.1dev";
 
 struct http_status {
 	char *code;
@@ -20,14 +23,15 @@ static const struct http_status OK = {
 };
 
 /*
- * Serve an HTTP 200 response with no headers.
+ * Serve an HTTP 200 response with basic headers.
  *
  * Response written to file descriptor fd, taking the body from body.
  */
 void http_res(int fd, char *body, int body_len)
 {
-	char c;
 	int req_body_len, n;
+	time_t req_instant;
+	struct tm *req_time;
 
 	/* setup RES_BUF to hold response */
 	n = RES_BUF_START;
@@ -35,14 +39,31 @@ void http_res(int fd, char *body, int body_len)
 	/* status line */
 	strcpy(RES_BUF + n, OK.code);
 	n += strlen(OK.code);
-	RES_BUF[n++] = ' ';
 
+	RES_BUF[n++] = ' ';
 	strcpy(RES_BUF + n, OK.msg);
 	n += strlen(OK.msg);
 
 	strcpy(RES_BUF + n, CRLF);
-	strcpy(RES_BUF + n + 2, CRLF);
-	n += 4;
+	n += 2;
+
+	/* headers */
+	req_instant = time(NULL);
+	req_time = gmtime(&req_instant);
+	n += strftime(RES_BUF + n, 100, "Date: %a, %d %b %Y %H:%M:%S GMT\r\n", req_time);
+
+	n += snprintf(RES_BUF + n, MAX_RESPONSE - n, "Server: %s\r\n", SERVER_TOKEN);
+	n += snprintf(RES_BUF + n, MAX_RESPONSE - n, "Content-Type: text/plain\r\n");
+	n += snprintf(RES_BUF + n, MAX_RESPONSE - n, "Content-Length: %d\r\n", body_len);
+	n += snprintf(RES_BUF + n, MAX_RESPONSE - n, "Cache-Control: no-cache\r\n");
+	n += snprintf(RES_BUF + n, MAX_RESPONSE - n, "Connection: close\r\n");
+
+	if (n + 2 >= MAX_RESPONSE) {
+		puts("RES_BUF full; aborting");
+		return;
+	}
+	strcpy(RES_BUF + n, CRLF);
+	n += 2;
 
 	/* body */
 	req_body_len = body_len < (MAX_RESPONSE - n) ? body_len : MAX_RESPONSE - n;
